5cbc52eda83e75eb8905e9a2b69159fca03807e0
EstevesDouglas <douglas.brsoftware@gmail.com>
2026-06-29 18:47:14 -0300

    atualiza identificação da distribuição e altera instalação do docker

--- install.sh
+++ install.sh
@@ -30,9 +30,12 @@ case "$ARCH_RAW" in
 esac
 
 # Identifica a distribuição em uso
-DISTRO=$(grep -Po '^ID_LIKE=\K.*' /etc/os-release)
+DISTRO=$(grep -Po '^ID=\K.*' /etc/os-release)
 case "$DISTRO" in
-    debian) DISTRO="debian-like" ;;
+    debian) DISTRO="debian" ;;
+    ubuntu) DISTRO="ubuntu" ;;
+    pop) DISTRO="pop" ;;
+    mint) DISTRO="mint" ;;
     *) DISTRO="unknown" ;;
 esac
 
@@ -51,13 +54,13 @@ fi )
 # Identifica a necessidade de instalar o docker
 docker run --rm hello-world &> /dev/null || (\
 if [ "$OS" == "linux" ]; then
-		if [ "$DISTRO" == "debian-like" ]; then
+		if [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "ubuntu"] ; then
     apt-get update
     apt-get install -y ca-certificates curl
     install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    curl -fsSL "https://download.docker.com/linux/${DISTRO}/gpg" -o /etc/apt/keyrings/docker.asc
     chmod a+r /etc/apt/keyrings/docker.asc
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] "https://download.docker.com/linux/${DISTRO}" \
       $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
     tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get update
